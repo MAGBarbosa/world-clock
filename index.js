40830fa3a4c15d134d646b0bc77b0f869ca10895
MAGBarbosa <138673494+MAGBarbosa@users.noreply.github.com>
2023-10-22 14:53:37 +0100

    Fixed bug where the time was not showing up

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ function updateTime() {
   let tokyoDateElement = tokyo.querySelector(".date");
   let tokyoTimeElement = tokyo.querySelector(".time");
 
-  let tokyoTime = moment().tz(Asia / Tokyo);
+  let tokyoTime = moment().tz("Asia/Tokyo");
 
   tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
-  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>A</small>]");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
 
   //Paris
   let paris = document.querySelector("#paris");
   let parisDateElement = paris.querySelector(".date");
   let parisTimeElement = paris.querySelector(".time");
 
-  let parisTime = moment().tz(Europe / Paris);
+  let parisTime = moment().tz("Europe/Paris");
 
   parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>A</small>]");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 
   //Sydney
 
@@ -25,21 +25,40 @@ function updateTime() {
   let sydneyDateElement = sydney.querySelector(".date");
   let sydneyTimeElement = sydney.querySelector(".time");
 
-  let sydneyTime = moment().tz(Australia / Sydney);
+  let sydneyTime = moment().tz("Australia/Sydney");
 
   sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
-  sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>A</small>]");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
   //London
   let london = document.querySelector("#london");
   let londonDateElement = london.querySelector(".date");
   let londonTimeElement = london.querySelector(".time");
 
-  let londonTime = moment().tz(Europe / london);
+  let londonTime = moment().tz("Europe/london");
 
   londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
-  londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>A</small>]");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+//Change background between morning and afternoon
+
+let background = document.querySelector(".container");
+background.style.backgroundRepeat = "no-repeat";
+background.style.backgroundSize = "cover";
+let dayHalf = moment().format("A");
+
+if (dayHalf === "AM") {
+  background.style.backgroundImage = "url('resources/daytime3.jpg')";
+  background.style.backgroundPosition = "top center";
+} else {
+  background.style.backgroundImage = "url('resources/img1.jpg')";
+  background.style.backgroundPosition = "bottom center";
+}
